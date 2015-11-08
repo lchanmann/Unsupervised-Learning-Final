3c18ecbcc501b9b82b6421c4047eb93319008a6e
Mann <chanmannlim@gmail.com>
2015-11-08 15:59:14 -0600

    use only subset of words

--- pre/mfc2mat.m
+++ pre/mfc2mat.m
@@ -5,13 +5,18 @@ close all;
 files = dir('tmp/mfc/*.mfc');
 C = length(files);
 MFCCs = cell(1, C);
+labels = zeros(C, 3);
 
 fprintf('Start converting mfc files');
 for i=1:C
     name = files(i).name;
     MFCCs{i} = readhtk_lite( ['tmp/mfc/' name] );
+    
+    % labels = [speaker gender word]
+    l = length(name);
+    labels(i,:) = [ceil(i/21) name(l-12) str2double(name(l - [5 4]))];
     progress(i, C);
 end
-save('../MFCCs.mat', 'MFCCs');
+save('../MFCCs.mat', 'MFCCs', 'labels');
 
 exit;
