6c003e76a1657808dbdc13dbe4be3f2324c271eb
Mann <chanmannlim@gmail.com>
2015-11-10 00:14:03 -0600

    print clustering result

--- words_clustering.m
+++ words_clustering.m
@@ -6,26 +6,55 @@ close all;
 load('MFCCs');
 
 % generate distance matrix
-N = 6;% length(MFCCs);
+N = length(MFCCs);
+% numbers of clusters
+m = 21;
+% DTW parameters
 w = 30;
 p = 1;
 
-% compare pdist and manually computed matrix
-d = pdist( MFCCs(1:N), dtw.new(w, p) );
-D = squareform(d)
-
 K = N*(N+1)/2 - N;
-dk = zeros(1, K);
+d = zeros(1, K);
 k = 1;
+tic;
+fprintf('Computing pairwise distance');
 for i=1:N
     for j=i:N
         if i ~= j
-            dk(k) = dtw.base(MFCCs{i}, MFCCs{j}, w, p);
+            d(k) = dtw.base(MFCCs{i}, MFCCs{j}, w, p);
             k = k + 1;
+            progress(k, K);
         end
     end
 end
-Dk = squareform(dk)
+fprintf('\nDone: ');
+toc
+
+Z = linkage(d, 'weighted');
+c = cluster(Z, 'maxclust', m);
+
+l = N/m;
+C = reshape(C, m, l)
+
+figure;
+dendrogram(Z);
+
+% % compare pdist and manually computed matrix
+% d = pdist( MFCCs(1:N), dtw.new(w, p) );
+% D = squareform(d)
+% 
+% K = N*(N+1)/2 - N;
+% dk = zeros(1, K);
+% k = 1;
+% for i=1:N
+%     for j=i:N
+%         if i ~= j
+%             dk(k) = dtw.base(MFCCs{i}, MFCCs{j}, w, p);
+%             k = k + 1;
+%         end
+%     end
+% end
+% Dk = squareform(dk)
 
 % tic;
 % disp('Computing pairwise distance...');
