88f29a26daf023093972166ae73a0f2d8ee60f32
Mann <chanmannlim@gmail.com>
2015-12-07 17:30:43 -0600

    generate speaker-based words distances

--- generate_d.m
+++ generate_d.m
@@ -11,6 +11,8 @@ N = length(MFCCs);
 % number of dimensions used
 l = 13;
 
+%% generate all words distances
+
 % DTW window parameter
 w = 50;
 
@@ -33,3 +35,34 @@ fprintf('done!\n');
 toc
 
 save('d13.mat', 'd');
+
+%% generate words distances by speaker
+
+w = 103;
+K = w*(w+1)/2 - w;
+d = zeros(1, K);
+
+for c=1:10
+    d = 0;
+    k = 1;
+    
+    from = (c-1) * w + 1;
+    to = from + w - 1;
+    
+    mfccs = MFCCs(from:to);
+    tic;
+    fprintf('\nSpeaker %d. Computing pairwise distance...', c);
+    for i=1:w
+        s = mfccs{i}(1:13);
+        for j=i+1:w
+            t = mfccs{j}(1:13);
+            d(k) = dtw_c(s, t, w);
+            k = k + 1;
+            progress(k, K);
+        end
+    end
+    fprintf('done!\n');
+    toc
+
+    save(['d_' num2str(c) '.mat'], 'd');
+end
