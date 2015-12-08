3439165ddf42f6eb7d4e9e4d147ac4bd2e6b833c
Fernando Torre Mora <torrefernand@gmail.com>
2015-12-07 18:26:07 -0600

    Bugfix

--- text_vs_speech.m
+++ text_vs_speech.m
@@ -7,7 +7,7 @@ w = length(pronounciations{1});
 
 h=waitbar(0,'Computing word distance');
 for i=1:w
-    h=waitbar((i(i+1)/2)/(w(w+1)/2),h,sprintf('Computing distance for word %d',i));
+    h=waitbar((i*(i+1)/2)/(w*(w+1)/2),h,sprintf('Computing distance for word %d',i));
     for j=i+1:w
         text_d(end+1) = strdist(pronounciations{1}{i},pronounciations{1}{j});
     end
@@ -15,15 +15,15 @@ end
 delete(h);
 Text_Z= linkage(text_d, 'weighted');
 
-% load data
-load('d_1.mat');
-
 % number of speakers
-s = 1;%10;
+s = 1;
 
 %%
 
-%for(i=1:s)
+for i=1:s
+    % load data
+    load(sprintf('d_%d.mat',i));
+    
     Speech_Z = linkage(d, 'weighted');
 
     a  =zeros(w,1);
@@ -34,14 +34,14 @@ s = 1;%10;
         a(m) = evaluate(speech_clustering,text_clustering);
         mi(m) = mutual_information(speech_clustering,text_clustering,'normalized');
     end
-    [best_accuracy, best_clustering] = max(a)
-    [worst_accuracy, worst_clustering] = min(a)
- %end
- 
- plot(a)
- hold on
- plot(mi)
- xlabel('Number of word clusters')
- ylabel('Ratio')
- title('Similarity of speech clustering compared to text clustering')
- legend('Ratio of matches','Mutual information score','Location','southeast')
+    
+    figure
+    plot(a)
+    hold on
+    plot(mi)
+    xlabel('Number of word clusters')
+    ylabel('Ratio')
+    title(sprintf('Similarity of speech clustering compared to text clustering for speaker %d',i))
+    legend('Ratio of matches','Mutual information score','Location','southeast')
+end
+
